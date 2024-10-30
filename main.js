d9bc55b8523cdf2f0e2aa32cdd34e4636b22c0ad
Naby-04 <smohamed.diop@gmail.com>
2024-10-30 18:17:48 +0100

    front-end finish rest js

---
+++ main.js
@@ -0,0 +1,23 @@
+var userData = [];
+let tableau = document.getElementById("table-body");
+let prenom = document.getElementById("user-FirstName");
+let nom = document.getElementById("user-Name");
+let email = document.getElementById("user-Email");
+let telephone = document.getElementById("num-telephone");
+
+function affichage() {
+  console.log("hello world");
+  // userData.push(
+  //   { Prenom: prenom },
+  //   { Nom: nom },
+  //   { Email: email },
+  //   { Telephone: telephone }
+  // );
+  // if (tableau) {
+  //   userData.forEach((element) => {
+  //     let row = document.createElement("tr");
+  //     row.innerHTML = `<td>${element.Prenom}</td><td>${element.Nom}</td><td>${element.Email}</td><td>${element.Telephone}</td>`;
+  //     tableau?.appendChild(row);
+  //   });
+  // }
+}
