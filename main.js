80d60a42758edddc40a3a6cd254270720f6daa2c
Naby-04 <smohamed.diop@gmail.com>
2024-11-08 15:25:21 +0100

    finish

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const PrenomInput = document.getElementById("user-FirstName");
 const NomInput = document.getElementById("user-Name");
 const EmailInput = document.getElementById("user-Email");
 const TelephoneInput = document.getElementById("num-telephone");
-const addBtn = document.getElementById("add");
+const addBtn = document.getElementById("addbtn");
 const editBtn = document.getElementById("edit");
 const rowEdit = document.getElementById("editbtn");
 const suppbtn = document.getElementById("supptbtn");
@@ -45,77 +45,87 @@ function updateTableau() {
   });
 }
 
-// fonction pour ajouter des utilisateurs
+let currentEditIndex = null; // Variable globale pour stocker l'index de l'utilisateur en cours de modification
+
+// Fonction pour ajouter ou modifier un utilisateur en fonction du contexte
 Form[0].addEventListener("submit", function addUser(e) {
   e.preventDefault();
+
   const user = {
     Prenom: PrenomInput.value,
     Nom: NomInput.value,
     Email: EmailInput.value,
     Telephone: TelephoneInput.value,
   };
-  // Ajouter l'utilisateur au tableau
-  usersData.push(user);
+
+  if (currentEditIndex === null) {
+    // Ajouter un nouvel utilisateur
+    usersData.push(user);
+  } else {
+    // Mettre à jour l'utilisateur existant
+    usersData[currentEditIndex] = user;
+  }
+
+  // Sauvegarder dans localStorage et mettre à jour le tableau
   localStorage.setItem("users", JSON.stringify(usersData));
   updateTableau();
 
-  // Vider les champs de formulaire après ajout
-  PrenomInput.value = "";
-  NomInput.value = "";
-  EmailInput.value = "";
-  TelephoneInput.value = "";
+  // Réinitialiser le formulaire et les boutons
+  resetForm();
 });
 
-// fonction pour supprimer un user
-function suppUser(index) {
-  usersData.splice(index, 1);
-  localStorage.setItem("users", JSON.stringify(usersData));
-  updateTableau();
-}
-
-// fonction pour modifier les infos d'un utilisateur
+// Fonction pour remplir le formulaire avec les informations d'un utilisateur pour édition
 function editUser(index) {
-  console.table(usersData); // Pour voir si le tableau est rempli correctement
-  console.table(usersData[index]); // Pour vérifier l'élément que tu veux modifier
+  const user = usersData[index];
+  if (user) {
+    // Remplir les champs avec les données existantes
+    PrenomInput.value = user.Prenom;
+    NomInput.value = user.Nom;
+    EmailInput.value = user.Email;
+    TelephoneInput.value = user.Telephone;
 
-  if (
-    usersData[index] &&
-    PrenomInput &&
-    NomInput &&
-    EmailInput &&
-    TelephoneInput
-  ) {
-    PrenomInput.value = usersData[index].Prenom;
-    NomInput.value = usersData[index].Nom;
-    EmailInput.value = usersData[index].Email;
-    TelephoneInput.value = usersData[index].Telephone;
-  } else {
-    console.log("Utilisateur non trouvé à l'index", index);
+    // Sauvegarder l'index de l'utilisateur en cours de modification
+    currentEditIndex = index;
+
+    // Afficher le bouton "Enregistrer" et masquer le bouton "Ajouter"
+    addBtn.style.display = "none";
+    editBtn.style.display = "block";
   }
-  addBtn.style.display = "none";
-  editBtn.style.display = "block";
+}
+// Fonction pour enregistrer les modifications d'un utilisateur
+function editUpdate() {
+  if (currentEditIndex !== null) {
+    // Mettre à jour les informations de l'utilisateur en cours de modification
+    usersData[currentEditIndex] = {
+      Prenom: PrenomInput.value,
+      Nom: NomInput.value,
+      Email: EmailInput.value,
+      Telephone: TelephoneInput.value,
+    };
+
+    // Sauvegarder les modifications dans le localStorage
+    localStorage.setItem("users", JSON.stringify(usersData));
+
+    // Mettre à jour l'affichage du tableau
+    updateTableau();
+
+    // Réinitialiser le formulaire et l'affichage des boutons
+    resetForm();
+  }
 }
 
-// fonction pour metre a jour le tableau apres les modifications
-function editUpdate(index) {
-  // Mettre à jour les données dans le tableau
-  usersData[index].Prenom = PrenomInput.value;
-  usersData[index].Nom = NomInput.value;
-  usersData[index].Email = EmailInput.value;
-  usersData[index].Telephone = TelephoneInput.value;
-
-  // Sauvegarder les modifications dans le localStorage
-  localStorage.setItem("users", JSON.stringify(usersData));
-
-  // Mettre à jour le tableau pour afficher les nouvelles données
-  updateTableau();
-
-  // Vider les champs de formulaire après ajout
+// Fonction pour réinitialiser le formulaire et les boutons
+function resetForm() {
+  // Réinitialiser les champs de saisie
   PrenomInput.value = "";
   NomInput.value = "";
   EmailInput.value = "";
   TelephoneInput.value = "";
 
+  // Réinitialiser l'affichage des boutons
   addBtn.style.display = "block";
   editBtn.style.display = "none";
+
+  // Réinitialiser l'index en cours de modification
+  currentEditIndex = null;
 }
